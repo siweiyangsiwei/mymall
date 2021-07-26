95d1b0300ea6965eb7ba991f4adb70858bfd82ac
yangsiweishishabi <913246788@qq.com>
2021-07-26 16:05:10 +0800

    2021/7/26

--- src/network/detail.js
+++ src/network/detail.js
@@ -7,3 +7,25 @@ export function getDetail(iid){
         }
     })
 }
+export class Goods {//定义一个类整合数据
+    constructor(itemInfo,columns,services){
+        this.title = itemInfo.title
+        this.desc = itemInfo.desc
+        this.newPrice = itemInfo.newPrice
+        this.oldPrice = itemInfo.oldPrice
+        this.discount = itemInfo.discountDesc
+        this.columns = columns
+        this.services = services
+        this.realPrice = itemInfo.lowNowPrice
+    }
+}
+export class Shop{
+    constructor(shopInfo){
+        this.logo = shopInfo.shopLogo
+        this.name = shopInfo.name
+        this.fans = shopInfo.cFans
+        this.sells = shopInfo.cSells
+        this.score = shopInfo.score
+        this.goodsCount = shopInfo.cGoods
+    }
+}
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,29 +2,44 @@
   <div id="detail">
       <detail-nav-bar></detail-nav-bar>
       <detail-swiper @top-images = 'topImages'></detail-swiper>
+      <detail-base-info></detail-base-info>
+      <detail-shop-info></detail-shop-info>
   </div>
 </template>
 
 <script>
 import DetailNavBar from './childComps/DetailNavBar'
 import DetailSwiper from './childComps/DetailSwiper'
-import {getDetail} from '../../network/detail'
+import DetailBaseInfo from './childComps/DetailBaseInfo'
+import DetailShopInfo from './childComps/DetailShopInfo'
+
+import {getDetail,Goods,Shop} from '../../network/detail'
 export default {
     components: {
-      DetailNavBar,
-      DetailSwiper
+         DetailNavBar,
+        DetailSwiper,
+        DetailBaseInfo,
+        DetailShopInfo
+        
     },
     data(){
         return{
             iid:null,
-            topImages:[]
+            topImages:[],
+            goods:null,
+            shop:null,
         }
     },
     created(){
         this.iid = this.$route.params.iid//通过路由获取iid
         getDetail(this.iid).then(res => {
             console.log(res)
-            // this.topImages = res.result.itemInfo.topImages
+            const data = res.result
+            // this.topImages = data.itemInfo.topImages获取轮播图中的图片
+            // this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)通过类获取商品介绍栏中的数据
+            // this.shop = new Shop(data.shopInfo)获取商家信息
+
+
         })
     }
 }
